ccd4699e4a245131169be570775b3c94c03fe5e1
Vegard Rogne <Vegardrogne@hotmail.no>
2018-09-03 21:24:38 +0200

    Moved all javascript from server html page into a seperate js file.

---
+++ src/main/resources/static/js/server.js
@@ -0,0 +1,258 @@
+<script>
+
+    function openDocument(link){
+    $("#doc").load(link);
+    }
+
+
+<!-- START -- Version text  -->
+	$(document).ready(function(){
+	    Version_Text();
+	});
+
+	function Version_Text(){
+	$.ajax({
+		url: "/version",
+		method: "GET",
+		success: function(data) {
+		    $('#version').empty();
+		    $('#version').html(data);
+		},
+		error: function(data) {
+			console.log(data);
+			$('#version').empty();
+		    $('#version').html(data);
+		}
+	});
+};
+<!-- END -- Version text -->
+
+<!-- START -- GET Platform Name  -->
+	$(document).ready(function(){
+	   getServerPlatformName();
+	});
+
+	function getServerPlatformName(){
+	$.ajax({
+		url: "/server/platformname",
+		method: "GET",
+		success: function(data) {
+		    $('#server_platformName').empty();
+		    $('#server_platformName').html("<p class=\"text-center\"> Platform name: " + data + "</p>");
+		},
+		error: function(e) {
+			$('#server_platformName').empty();
+		    $('#server_platformName').html(e.responseText);
+		}
+	});
+};
+<!-- END -- GET Platform Name  -->
+
+<!-- START -- GET Serial Number  -->
+	$(document).ready(function(){
+	   getServerSerialNumber();
+	});
+
+	function getServerSerialNumber(){
+	$.ajax({
+		url: "/server/serialnumber",
+		method: "GET",
+		success: function(data) {
+		    $('#server_hardwareInfo').append("<p class=\"text-center\"> Serial number: " + data + "</p>");
+		},
+		error: function(e) {
+		    $('#server_hardwareInfo').html(e.responseText);
+		}
+	});
+};
+<!-- END -- GET Serial Number  -->
+
+
+<!-- START -- GET CPU Revision  -->
+	$(document).ready(function(){
+	   getServerCPURevision();
+	});
+
+	function getServerCPURevision(){
+	$.ajax({
+		url: "/server/cpurevision",
+		method: "GET",
+		success: function(data) {
+		    $('#server_hardwareInfo').append("<p class=\"text-center\"> CPU Revision: " + data + "</p>");
+		},
+		error: function(e) {
+		    $('#server_hardwareInfo').html(e.responseText);
+		}
+	});
+};
+<!-- END -- GET CPU Revision  -->
+
+<!-- START -- GET CPU Architecture  -->
+	$(document).ready(function(){
+	   getServerCPUArchitecture();
+	});
+
+	function getServerCPUArchitecture(){
+	$.ajax({
+		url: "/server/cpuarchitecture",
+		method: "GET",
+		success: function(data) {
+		    $('#server_hardwareInfo').append("<p class=\"text-center\"> CPU Architecture: " + data + "</p>");
+		},
+		error: function(e) {
+		    $('#server_hardwareInfo').html(e.responseText);
+		}
+	});
+};
+<!-- END -- GET CPU Architecture  -->
+
+<!-- START -- GET CPU Architecture  -->
+	$(document).ready(function(){
+	   getServerCPUPart();
+	});
+
+	function getServerCPUPart(){
+	$.ajax({
+		url: "/server/cpupart",
+		method: "GET",
+		success: function(data) {
+		    $('#server_hardwareInfo').append("<p class=\"text-center\"> CPU Part: " + data + "</p>");
+		},
+		error: function(e) {
+		    $('#server_hardwareInfo').html(e.responseText);
+		}
+	});
+};
+<!-- END -- GET CPU Architecture  -->
+
+<!-- START -- GET CPU Architecture  -->
+	$(document).ready(function(){
+	   getServerCPUVoltage();
+	});
+
+	function getServerCPUVoltage(){
+	$.ajax({
+		url: "/server/cpuvoltage",
+		method: "GET",
+		success: function(data) {
+		    $('#server_hardwareInfo').append("<p class=\"text-center\"> CPU Voltage: " + data + "</p>");
+		},
+		error: function(e) {
+		    $('#server_hardwareInfo').html(e.responseText);
+		}
+	});
+};
+<!-- END -- GET CPU Architecture  -->
+
+<!-- START -- GET Server Fan Running  -->
+	$(document).ready(function(){
+	   getServerFanRunning();
+	   setInterval(getServerFanRunning, 1000);
+	});
+
+	function getServerFanRunning(){
+	$.ajax({
+		url: "/serverFan/output",
+		method: "GET",
+		success: function(data) {
+		    $('#serverFan_running').empty();
+		    if(data == "true"){
+		    $('#serverFan_running').empty();
+		    $('#serverFan_running').html("<img src=\"/image/fan_icon_on.png\" class=\"img-responsive center-block\">");
+		    }
+		    else{
+		    $('#serverFan_running').empty();
+		    $('#serverFan_running').html("<img src=\"/image/fan_icon_off.png\" class=\"img-responsive center-block\">");
+		    }
+
+		},
+		error: function(e) {
+			$('#serverFan_running').empty();
+		    $('#serverFan_running').html(e.responseText);
+		}
+	});
+};
+<!-- END -- GET Server Fan Running  -->
+
+<!-- START -- GET Server CPU Temperature  -->
+	$(document).ready(function(){
+	   getServerCPUTemp();
+	   setInterval(getServerCPUTemp, 1000);
+	});
+
+	function getServerCPUTemp(){
+	$.ajax({
+		url: "/server/cputemperature",
+		method: "GET",
+		success: function(data) {
+		    $('#serverFan_cpuTemp').empty();
+		    $('#serverFan_cpuTemp').html("<h4 class=\"text-center\"> CPU temperature: " + data + " °C</h4>");
+		    $('#serverFan_cpuTempInfo').empty();
+		    $('#serverFan_cpuTempInfo').html("<p class=\"text-center\"> CPU temperature: " + data + " °C</p>");
+		},
+		error: function(e) {
+			$('#serverFan_cpuTemp').empty();
+		    $('#serverFan_cpuTemp').html(e.responseText);
+		}
+	});
+};
+<!-- END -- GET Server CPU Temperature -->
+
+<!-- START -- GET ServerFan setpoint  -->
+	$(document).ready(function(){
+	   getServerFanSetpoint();
+	});
+
+	function getServerFanSetpoint(){
+	$.ajax({
+		url: "/serverFan/setpoint",
+		method: "GET",
+		success: function(data) {
+		    $('#serverFan_setpoint').empty();
+		    $('#serverFan_setpoint').html("<h4 class=\"text-center\"> Setpoint: " + data + " °C</h4>");
+		},
+		error: function(data) {
+			console.log(data);
+			$('#serverFan_setpoint').empty();
+		    $('#serverFan_setpoint').html(data);
+		}
+	});
+};
+<!-- END -- GET ServerFan setpoint -->
+
+<!-- START -- UPDATE ServerFan setpoint -->
+$( document ).ready(function() {
+
+	// SUBMIT FORM
+    $("#serverFan_setpoint_update").submit(function(event) {
+		// Prevent the form from submitting via the browser.
+		event.preventDefault();
+		setServerFanSetpoint();
+	});
+
+
+    function setServerFanSetpoint(){
+    	var formData = {
+    		setpoint : $("#serverFan_setpoint_update_value").val(),
+    	}
+    	$.ajax({
+			type : "POST",
+			contentType : "application/json",
+			url : "/serverfan/updateSetpoint",
+			data : JSON.stringify(formData),
+			success : function(result) {
+				$("#serverFan_setpoint_update_text").html(result);
+				getServerFanSetpoint();
+			},
+			error : function(e) {
+			console.log(e);
+	            $("#serverFan_setpoint_update_text").html(e.responseText);
+			}
+		});
+    }
+})
+<!-- END -- UPDATE ServerFan setpoint -->
+
+</script>
+
+
