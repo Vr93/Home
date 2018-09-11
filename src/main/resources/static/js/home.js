e652fc2420300d4f1c9be5203463a212038320f9
Vegard Rogne <Vegardrogne@hotmail.no>
2018-09-11 23:31:57 +0200

    Added last values from A01 weather station to index page.

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 Version_Text();
+A01_isOnline();
+A01_lastValues();
 
 
 function Version_Text(){
@@ -14,6 +16,53 @@ function Version_Text(){
 			console.log(data);
 			$('#version').empty();
 		    $('#version').html(data);
+		}
+	});
+};
+
+function A01_lastValues(){
+	$.ajax({
+		url: "/A01/lastvalues",
+		method: "GET",
+		success: function(data) {
+		    var time;
+			var temperature;
+			var humidity;
+			var pressure;
+            var obj = JSON.parse(data);
+            temperature = obj.temp;
+            humidity = obj.humidity;
+            pressure = obj.pressure;
+            time = obj.timestamp;
+
+        $('#A01_lastValues').empty();
+        var data = "<h3 class=\"text-center\">" + temperature + " °C </p>";
+        data = data + "<h3 class=\"text-center\">" + humidity + " % </p>";
+        data = data + "<h3 class=\"text-center\">" + pressure + " hPa </p>";
+        data = data + "<h3 class=\"text-center\">" + time + "</p>";
+        $('#A01_lastValues').html(data);
+		},
+		error: function(data) {
+			$('#A01_isOnline').empty();
+		    $('#A01_isOnline').html(data.responseText);
+		}
+	});
+};
+
+function A01_isOnline(){
+	$.ajax({
+		url: "/A01/isonline",
+		method: "GET",
+		success: function(data) {
+		    $('#A01_isOnline').empty();
+		    if(data == "false"){
+		    $('#A01_isOnline').html("<p class=\"text-center text-danger\"> Device is offline! </p>");
+		    }
+		},
+		error: function(data) {
+			console.log(data);
+			$('#A01_isOnline').empty();
+		    $('#A01_isOnline').html(data.responseText);
 		}
 	});
 };
