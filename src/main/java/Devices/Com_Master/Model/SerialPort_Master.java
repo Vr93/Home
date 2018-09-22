7662a2f9c83cad88fc9eb7b78ef688b900840ff3
Vegard Rogne <Vegardrogne@hotmail.no>
2018-09-22 20:11:54 +0200

    Fixed server, data storage information. Now shows in a table on server page. Experimental with JSSC library for serial communication, not working yet.

--- src/main/java/Devices/Com_Master/Model/SerialPort_Master.java
+++ src/main/java/Devices/Com_Master/Model/SerialPort_Master.java
@@ -1,9 +1,16 @@
 package Devices.Com_Master.Model;
 
 
+
+import jssc.SerialPort;
+import jssc.SerialPortEvent;
+import jssc.SerialPortEventListener;
+import jssc.SerialPortException;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 /**
  * This example code demonstrates how to perform serial communications using the Raspberry Pi.
@@ -12,6 +19,74 @@ import java.io.IOException;
  */
 @Service
 public class SerialPort_Master {
+    private static SerialPort serialPort;
+    public SerialPort_Master(){
+        connect();
+    }
+
+    private void connect(){
+        serialPort = new SerialPort("/dev/ttyAMA0");
+        try {
+            serialPort.openPort();
+            System.out.println("Serial port is opened: " + serialPort.getPortName());
+            serialPort.setParams(9600, 8, 1, 0);
+            //Preparing a mask. In a mask, we need to specify the types of events that we want to track.
+            //Well, for example, we need to know what came some data, thus in the mask must have the
+            //following value: MASK_RXCHAR. If we, for example, still need to know about changes in states
+            //of lines CTS and DSR, the mask has to look like this: SerialPort.MASK_RXCHAR + SerialPort.MASK_CTS + SerialPort.MASK_DSR
+            int mask = SerialPort.MASK_RXCHAR;
+            //Set the prepared mask
+            serialPort.setEventsMask(mask);
+            //Add an interface through which we will receive information about events
+            serialPort.addEventListener(new SerialPortReader());
+
+            //serialPort.writeString("HelloWorld");
+        }
+        catch (SerialPortException ex) {
+            System.out.println(ex);
+        }
+    }
+
+
+    static class SerialPortReader implements SerialPortEventListener {
+        @Override
+        public void serialEvent(SerialPortEvent event) {
+            //Object type SerialPortEvent carries information about which event occurred and a value.
+            //For example, if the data came a method event.getEventValue() returns us the number of bytes in the input buffer.
+            System.out.println(event.getEventType());
+            if(event.isRXCHAR()){
+                if(event.getEventValue() == 10){
+                    try {
+                        String data= serialPort.readString();
+                        System.out.println(data);
+                    }
+                    catch (SerialPortException ex) {
+                        System.out.println(ex);
+                    }
+                }
+            }
+            //If the CTS line status has changed, then the method event.getEventValue() returns 1 if the line is ON and 0 if it is OFF.
+            else if(event.isCTS()){
+                if(event.getEventValue() == 1){
+                    System.out.println("CTS - ON");
+                }
+                else {
+                    System.out.println("CTS - OFF");
+                }
+            }
+            else if(event.isDSR()){
+                if(event.getEventValue() == 1){
+                    System.out.println("DSR - ON");
+                }
+                else {
+                    System.out.println("DSR - OFF");
+                }
+            }
+        }
+    }
+
+
+
 
 
 }
--- src/main/java/Server/Model/ServerInformation.java
+++ src/main/java/Server/Model/ServerInformation.java
@@ -2,6 +2,7 @@ package Server.Model;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ServerInformation {
 
@@ -22,25 +23,33 @@ public class ServerInformation {
     }
 
     public static String[] getServerStorage(){
-        ArrayList<String> arrList = Shell.doShellCommand("df -Bm");
-        String[] data = new String[arrList.size()];
+        try {
+            ArrayList<String> arrList = Shell.doShellCommand("df -Bm");
+            String[] data = new String[arrList.size()];
+            for(int x = 0; x < data.length; x++){
+                data[x] = "";
+            }
         /* The data from shell is unorganized, with many whitespaces etc,
         Reorganize the data and store it in string array. */
-        for(int i = 0; i < data.length; i++){
-            System.out.println(i);
-            String[] split = arrList.get(i).split( " ");
-            for(String splitData: split) {
-                if(!splitData.equalsIgnoreCase("")) {
-                    if(!data[i].equalsIgnoreCase("")) {
-                        data[i] = data[i] + "," + splitData;
-                    }
-                    else{
-                        data[i] = splitData;
+            for (int i = 0; i < arrList.size(); i++) {
+                String[] split = arrList.get(i).split(" ");
+                for (String splitData : split) {
+                    if (!splitData.equalsIgnoreCase("")) {
+                        if (!data[i].equalsIgnoreCase("")) {
+                            data[i] = data[i] + "," + splitData;
+                        } else {
+                            data[i] = splitData;
+                        }
                     }
                 }
             }
+            return data;
         }
-        return data;
+        catch(Exception ex){
+            String error[] = new String[1];
+            error[0] = "error";
+            return error;
+        }
     }
 
 
